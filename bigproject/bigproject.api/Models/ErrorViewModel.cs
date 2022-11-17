1d1ce6e319a7a12712cc8db86453024f4487d760
Endurance-Peter <endurance.peter@cyphercrescent.com>
2022-11-17 09:56:23 +0100

    added project for razor page

---
+++ bigproject/bigproject.api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bigproject.api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
